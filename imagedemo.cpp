0d3bde694ffbebb14ee1ce6b925eb027efeb93fa
Maraj09 <md.marajrashid@gmail.com>
2021-09-24 18:35:12 +0600

    some more features added

--- imagedemo.cpp
+++ imagedemo.cpp
@@ -1,18 +1,32 @@
 #include "iGraphics.h"
 
 int ground_width = 800, ground_height = 600, ground_width_start = 240, ground_height_start = 60, ground_width_end = ground_width + ground_width_start, ground_height_end = ground_height + ground_height_start;
-int image2, ground_img;
+int overlay, ground_img, side_logo, sound_on, sound_off, toogle_music = 1, reset_img;
 int snake_head_bottom, snake_head_top, snake_head_left, snake_head_right, snake_body_x, snake_body_y, snake_tail, fruit;
 int snake_width = 20, snake_height = 20;
-int x[1000], y[1000], d = 1, length = 10, dir = 1, p_d = 0;
-int snake_speed = 12, stop_game = 0;
-int rx = 320, ry = 320, f = 1;
-int gg[3];
+int x[2000], y[2000], d = 1, length = 50, dir = 1, p_d = 0;
+int snake_speed = 12, game_over = 0;
+int rx = 320, ry = 320, f = 0;
+char t_score[50];
+char score[5];
 
 void iDraw()
 {
-	iClear();
 
+	if (toogle_music == 1)
+	{
+
+		mciSendString(TEXT("resume  mp3"), NULL, 0, NULL);
+	}
+	else
+	{
+		mciSendString(TEXT("pause  mp3"), NULL, 0, NULL);
+	}
+	if (game_over == 1)
+	{
+		mciSendString(TEXT("stop mp3"), NULL, 0, NULL);
+	}
+	iClear();
 	iSetColor(30, 30, 30);
 	iFilledRectangle(0, 0, 1280, 720);
 	iShowImage(ground_width_start, ground_height_start, ground_width, ground_height, ground_img);
@@ -21,7 +35,28 @@ void iDraw()
 	iSetColor(255, 255, 255);
 	iText(10, 10, "Press p for pause, r for resume, END for exit.");
 	iSetColor(255, 0, 0);
-
+	// Game UI
+	iShowImage(10, 450, 200, 200, side_logo);
+	iSetColor(255, 255, 255);
+	iText(75, 420, "Music", GLUT_BITMAP_9_BY_15);
+	if (toogle_music == 1)
+	{
+		iShowImage(75, 350, 50, 50, sound_on);
+	}
+	else
+	{
+		iShowImage(75, 350, 50, 50, sound_off);
+	}
+	//	Score Board
+	iSetColor(102, 0, 255);
+	iText(1100, 650, "ScoreBoard", GLUT_BITMAP_TIMES_ROMAN_24);
+	iSetColor(255, 102, 0);
+	iText(1125, 610, "LEVEL 1", GLUT_BITMAP_HELVETICA_18);
+	iSetColor(51, 153, 0);
+	iText(1075, 550, "Your Current Score", GLUT_BITMAP_9_BY_15);
+	iSetColor(255, 0, 0);
+	sprintf(score, "%d", f);
+	iText(1150, 525, score, GLUT_BITMAP_9_BY_15);
 	//////////////////////////////////////////////////////////////
 	// FOR SNAKE MOVEMENT-Start
 	//////////////////////////////////////////////////////////////
@@ -92,10 +127,9 @@ void iDraw()
 				iShowImage(x[i] - 5, y[i], snake_width + 10, snake_height, snake_head_top);
 			}
 		}
-		// iFilledRectangle(x[i], y[i], snake_width, snake_height);
 	}
 
-	if (stop_game == 0)
+	if (game_over == 0)
 	{
 
 		for (int i = 999; i > 0; i--)
@@ -114,172 +148,182 @@ void iDraw()
 	if (x[0] > rx - 20 && x[0] < rx + 20 && y[0] > ry - 20 && y[0] < ry + 20)
 	{
 
-		length += 10;
+		length += 5;
 		f = f + 1;
 		PlaySound(TEXT("music//food.wav"), NULL, SND_ASYNC);
 		rx = (rand() % (ground_width_end - ground_width_start)) + ground_width_start;
 		ry = (rand() % (ground_height_end - ground_height_start)) + ground_height_start;
 	}
-	// iFilledRectangle(rx, ry, 23, 22);
 	iShowImage(rx, ry, 30, 30, fruit);
 	//////////////////////////////////////////////////////////////
 	// FOR Fruit-End
 	//////////////////////////////////////////////////////////////
-	// iFilledRectangle(50,50,1,1);
-	// iGetPixelColor(50,50 , gg);
-	// int c_c[] = {55, 0, 0};
-	// if(memcmp(gg, c_c, sizeof(c_c)) == 0 ){
-	// 	printf("gg");
-	// }else{
-	// 	printf("sddd");
-	// }
+	sprintf(t_score, "Your Score is: %d", f);
+	//////////////////////////////////////////////////////////////
+	// Game Over-Start
+	//////////////////////////////////////////////////////////////
+	if (game_over == 1)
+	{
+		iSetColor(255, 0, 0);
+		iFilledRectangle(385, 300, 500, 300);
+		iSetColor(255, 255, 255);
+		iText(575, 500, "GAME OVER!", GLUT_BITMAP_TIMES_ROMAN_24);
+		iText(560, 470, "OPPS! You Eat YourSelf", GLUT_BITMAP_9_BY_15);
+		iText(575, 425, t_score, GLUT_BITMAP_HELVETICA_18);
+		iShowImage(615, 325, 60, 60, reset_img);
+	}
 }
 
 void snake_movement()
 {
-	switch (d)
+	if (game_over == 0)
 	{
-	case 0:
-		if (dir == 1)
+
+		switch (d)
 		{
-			if (x[0] + snake_width >= ground_width_end)
+		case 0:
+			if (dir == 1)
 			{
-				x[0] = ground_width_start;
+				if (x[0] + snake_width >= ground_width_end)
+				{
+					x[0] = ground_width_start;
+				}
+				else
+				{
+					x[0] = x[0] + snake_speed;
+					p_d = 1;
+				}
+			}
+			else if (dir == 2)
+			{
+				if (x[0] <= ground_width_start)
+				{
+					x[0] = ground_width_end - snake_width;
+				}
+				else
+				{
+					x[0] = x[0] - snake_speed;
+					p_d = 2;
+				}
+			}
+			else if (dir == 3)
+			{
+				if (y[0] <= ground_height_start + 10)
+				{
+					y[0] = ground_height_end - snake_height;
+				}
+				else
+				{
+					y[0] = y[0] - snake_speed;
+					p_d = 3;
+				}
+			}
+			else if (dir == 4)
+			{
+				if (y[0] + snake_height + 10 >= ground_height_end)
+				{
+					y[0] = ground_height_start;
+				}
+				else
+				{
+					y[0] = y[0] + snake_speed;
+					p_d = 4;
+				}
 			}
 			else
 			{
-				x[0] = x[0] + snake_speed;
-				p_d = 1;
+				d = 0;
 			}
-		}
-		else if (dir == 2)
-		{
-			if (x[0] <= ground_width_start)
+			break;
+		case 1:
+			if (p_d != 2)
 			{
-				x[0] = ground_width_end - snake_width;
+				if (x[0] + snake_width + 10 >= ground_width_end)
+				{
+					x[0] = ground_width_start;
+				}
+				else
+				{
+					dir = 1;
+					x[0] = x[0] + snake_speed;
+				}
 			}
 			else
 			{
+
+				dir = 2;
 				x[0] = x[0] - snake_speed;
-				p_d = 2;
 			}
-		}
-		else if (dir == 3)
-		{
-			if (y[0] <= ground_height_start + 10)
+			break;
+		case 2:
+			if (p_d != 1)
 			{
-				y[0] = ground_height_end - snake_height;
-			}
-			else
-			{
-				y[0] = y[0] - snake_speed;
-				p_d = 3;
-			}
-		}
-		else if (dir == 4)
-		{
-			if (y[0] + snake_height + 10 >= ground_height_end )
-			{
-				y[0] = ground_height_start;
-			}
-			else
-			{
-				y[0] = y[0] + snake_speed;
-				p_d = 4;
-			}
-		}
-		else
-		{
-			d = 0;
-		}
-		break;
-	case 1:
-		if (p_d != 2)
-		{
-			if (x[0] + snake_width + 10 >= ground_width_end)
-			{
-				x[0] = ground_width_start;
+				if (x[0] <= ground_width_start)
+				{
+					x[0] = ground_width_end - snake_width;
+				}
+				else
+				{
+					x[0] = x[0] - snake_speed;
+					dir = 2;
+				}
 			}
 			else
 			{
 				dir = 1;
 				x[0] = x[0] + snake_speed;
 			}
-		}
-		else
-		{
-
-			dir = 2;
-			x[0] = x[0] - snake_speed;
-		}
-		break;
-	case 2:
-		if (p_d != 1)
-		{
-			if (x[0] <= ground_width_start)
+			break;
+		case 3:
+			if (p_d != 4)
 			{
-				x[0] = ground_width_end - snake_width;
-			}
-			else
-			{
-				x[0] = x[0] - snake_speed;
-				dir = 2;
-			}
-		}
-		else
-		{
-			dir = 1;
-			x[0] = x[0] + snake_speed;
-		}
-		break;
-	case 3:
-		if (p_d != 4)
-		{
-			if (y[0] <= ground_height_start + 10)
-			{
-				y[0] = ground_height_end - snake_height;
-			}
-			else
-			{
-				dir = 3;
-				y[0] = y[0] - snake_speed;
-			}
-		}
-		else
-		{
-			dir = 4;
-			y[0] = y[0] + snake_speed;
-		}
-
-		break;
-	case 4:
-		if (p_d != 3)
-		{
-			if (y[0] + snake_height + 10 >= ground_height_end)
-			{
-				y[0] = ground_height_start;
+				if (y[0] <= ground_height_start + 10)
+				{
+					y[0] = ground_height_end - snake_height;
+				}
+				else
+				{
+					dir = 3;
+					y[0] = y[0] - snake_speed;
+				}
 			}
 			else
 			{
 				dir = 4;
 				y[0] = y[0] + snake_speed;
 			}
-		}
-		else
-		{
-			dir = 3;
-			y[0] = y[0] - snake_speed;
-		}
 
-		break;
-	}
-	for (int i = 2; i < length; i++)
-	{
-		if (x[0] == x[i] && y[0] == y[i])
-		{
-			iPauseTimer(0);
-			stop_game = 1;
 			break;
+		case 4:
+			if (p_d != 3)
+			{
+				if (y[0] + snake_height + 10 >= ground_height_end)
+				{
+					y[0] = ground_height_start;
+				}
+				else
+				{
+					dir = 4;
+					y[0] = y[0] + snake_speed;
+				}
+			}
+			else
+			{
+				dir = 3;
+				y[0] = y[0] - snake_speed;
+			}
+
+			break;
+		}
+		for (int i = 2; i < length; i++)
+		{
+			if (x[0] == x[i] && y[0] == y[i])
+			{
+				iPauseTimer(0);
+				PlaySound(TEXT("music//gameover.wav"), NULL, SND_SYNC);
+				game_over = 1;
+				break;
+			}
 		}
 	}
 }
@@ -291,7 +335,6 @@ function iMouseMove() is called when the user presses and drags the mouse.
 void iMouseMove(int mx, int my)
 {
 	//place your codes here
-	printf("%d%d\n", mx, my);
 }
 
 /*
@@ -311,7 +354,31 @@ void iMouse(int button, int state, int mx, int my)
 {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
-		//place your codes here
+		printf("%d%d%d\n", mx, my, toogle_music);
+		if (mx >= 75 && mx <= 125 && my >= 350 && my <= 400)
+		{
+			if (toogle_music == 0)
+			{
+				toogle_music = 1;
+			}
+			else
+			{
+				toogle_music = 0;
+			}
+		}
+		if (mx >= 615 && mx <= 675 && my >= 325 && my <= 385)
+		{
+			if (game_over == 1)
+			{
+				game_over = 0;
+				iResumeTimer(0);
+				length = 10;
+				x[0] = 300;
+				y[0] = 300;
+				f = 0;
+				d = 1;
+			}
+		}
 	}
 	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
 	{
@@ -333,9 +400,12 @@ void iKeyboard(unsigned char key)
 	}
 	if (key == 'r')
 	{
-		iResumeTimer(0);
+		if (game_over != 1)
+		{
+			iResumeTimer(0);
+		}
 	}
-	if (key == 'w' || key == 's' || key == 'a' || key == 'd')
+	if (key == 'w' || key == 's' || key == 'a' || key == 'd' && game_over == 0)
 	{
 		PlaySound(TEXT("music//move.wav"), NULL, SND_ASYNC);
 	}
@@ -359,15 +429,16 @@ void iSpecialKeyboard(unsigned char key)
 	{
 		exit(0);
 	}
-	if (key == GLUT_KEY_LEFT || key == GLUT_KEY_UP || key == GLUT_KEY_RIGHT || key == GLUT_KEY_DOWN)
+	if (key == GLUT_KEY_LEFT || key == GLUT_KEY_UP || key == GLUT_KEY_RIGHT || key == GLUT_KEY_DOWN && game_over == 0)
 	{
 		PlaySound(TEXT("music//move.wav"), NULL, SND_ASYNC);
-		
 	}
 
 	//place your codes for other keys here
 }
-
+// void playSound(){
+// 	PlaySound(TEXT("music//music.wav"), NULL, SND_ASYNC|SND_NOSTOP);
+// }
 int main()
 {
 	//place your own initialization codes here.
@@ -375,7 +446,8 @@ int main()
 
 	iSetColor(255, 255, 255);
 	iInitialize(1280, 720, "Gg Snake");
-
+	mciSendString(TEXT("open \"music//music_2.mp3\" type mpegvideo alias mp3"), NULL, 0, NULL);
+	mciSendString(TEXT("play mp3 repeat"), NULL, 0, NULL);
 	// loading images.
 	/* iLoadImage() will return an interger type id
 	   for each image you load.
@@ -390,16 +462,17 @@ int main()
 	snake_tail = iLoadImage("images\\tail.png");
 	fruit = iLoadImage("images\\apple1.png");
 	ground_img = iLoadImage("images\\board.png");
+	side_logo = iLoadImage("images\\logo.png");
+	sound_on = iLoadImage("images\\volume.png");
+	sound_off = iLoadImage("images\\no-sound.png");
+	reset_img = iLoadImage("images\\reset.png");
 
 	for (int i = 0; i < 1000; i++)
 	{
 		x[i] = 0;
 		y[i] = 0;
 	}
-	for (int i = 0; i < 3; i++)
-	{
-		gg[i] = 0;
-	}
+
 	x[0] = 300;
 	y[0] = 300;
 
